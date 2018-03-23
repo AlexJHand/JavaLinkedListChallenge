bd4a3186847b51c241267b5e368cbcfc6882bb13
Alex Hand <alex.j.hand@gmail.com>
2018-03-23 13:54:28 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Create a program that implements a playlist for songs
+        // Create a song class having Title and Duration for a song
+        // The program will have an Album class containing a list of songs
+        // The albums will be stored in an ArrayList
+        // Songs from different albums can be added to the playlist and will appear in the list in the order
+        // they are added.
+        // Once the songs have been added to the playlist, create a menu of options to:
+        // Quit, Skip forward, skip backwards, replay the current song, and list the songs in the playlist.
+        // A song must exist in an album before it can be added to the playlist (so you can only play songs that
+        // you own).
+        // Hint: to replay a song, consider what happened when we went back and forth from a city before we
+        // started tracking the direction we were going in.
+        // As an optional extra, provide an option to remove the song from the playlist.
+    }
+}
